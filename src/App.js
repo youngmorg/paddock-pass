82622eb8867a0ce99429132f52bca11a14ba21f6
youngmorg <morganpmraynal@gmail.com>
2026-05-30 20:31:23 +0200

    Fix eslint comment syntax

--- src/App.js
+++ src/App.js
@@ -343,7 +343,7 @@ export default function App({ session }) {
       });
   }, [session]);
 
-  # eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line no-unused-vars
   const handleHideEvent = async (id) => {
     if (!session) { setAuthOpen(true); return; }
     setHiddenEvents(h => ({ ...h, [id]: true, [String(id)]: true }));
